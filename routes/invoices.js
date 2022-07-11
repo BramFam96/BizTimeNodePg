5dda9b7c731fe6ae9732934b442f520d1317ca63
BramFam96 <mtbramlett25@gmail.com>
2022-07-10 23:28:52 -0500

    removed errHandler func

--- routes/invoices.js
+++ routes/invoices.js
@@ -12,7 +12,7 @@ const dataExists = (data) => {
 }
 router.get('/', async (req, res, next) => {
 	try {
-		const results = await db.query('SELECT * FROM invoices')
+		const results = await db.query('SELECT id, comp_code, amt FROM invoices')
 		return res.json({ invoices: results.rows })
 	} catch (e) {
 		return next(e)
@@ -24,11 +24,10 @@ router.get('/:id', async (req, res, next) => {
 		const results = await db.query('SELECT * FROM invoices WHERE id = $1', [
 			req.params.id,
 		])
-		if (dataExists(results.rows)) {
-			return res.json({ invoices: results.rows[0] })
-		} else {
-			throw new ExpressError(`Can't find invoice with id of ${id}`, 404)
+		if (results.rows.length === 0) {
+			throw new ExpressError(`Can't find invoice with id of ${req.params.id}`, 404)
 		}
+		return res.json({ invoice: results.rows[0] })
 	} catch (e) {
 		return next(e)
 	}
@@ -36,11 +35,12 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
 	try {
+		const { comp_code, amt } = req.body
 		const results = await db.query(
 			'INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date',
 			[comp_code, amt]
 		)
-		return res.status(201).json(results.rows[0])
+		return res.status(201).json({ invoice: results.rows[0] })
 	} catch (e) {
 		return next(e)
 	}
@@ -48,24 +48,23 @@ router.post('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
 	try {
-    // Getting data
-    // #######################################################
-    // Get params
 		const { amt, paid } = req.body
 		const { id } = req.params
-    // Get invoice 
+		// Get invoice
 		const invoiceData = await db.query(
-		  `SELECT * FROM invoices 
-       WHERE id = $1`,[id])
-    // Check invoice exists:
-		if (dataExists(invoiceData.rows)) {
-			throw new ExpressError(`Can't find invoice with id of ${id}`, 404)
+			`SELECT * FROM invoices 
+       WHERE id = $1`,
+			[id]
+		)
+		// Check invoice exists:
+		if (invoiceData.rows.length === 0) {
+			throw new ExpressError(`Can't find invoice with id of ${req.params.id}`, 404)
 		}
-// ##########################################################
-// Logic
-		let paidDate = invoiceData.rows[0].paid_date
+		// ##########################################################
+		// Logic
+		let { paidDate } = invoiceData.rows[0]
 
-		if (!paidDate && data.paid) {
+		if (!paidDate && paid) {
 			paidDate = new Date()
 		} else if (!paid) {
 			paidDate = null
@@ -86,4 +85,24 @@ router.put('/:id', async (req, res, next) => {
 		return next(e)
 	}
 })
+
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const result = await db.query(
+			'DELETE FROM invoices WHERE id = $1 RETURNING id',
+			[req.params.id]
+		)
+
+		if (result.rows.length === 0) {
+			throw new ExpressError(
+				`There is no invoice with id of '${req.params.id}`,
+				404
+			)
+		}
+		return res.json({ success: 'invoice deleted' })
+	} catch (err) {
+		return next(err)
+	}
+})
+
 module.exports = router
